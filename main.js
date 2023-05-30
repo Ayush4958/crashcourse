4793111a2f2cf8e84df976de6af4ad043d48c5b8
Ayush4958 <98166557+Ayush4958@users.noreply.github.com>
2023-05-30 07:04:23 +0530

    Crash Course Website

---
+++ main.js
@@ -0,0 +1,14 @@
+let btn  = document.querySelector('#btn');
+let nav = document.querySelector('.navbar')
+let search = document.querySelector('.bx-search')
+
+btn.onclick = function() {
+    nav.classList.toggle("active")
+}
+
+search.onclick = function() {
+    nav.classList.toggle("active")
+}
+
+
+
